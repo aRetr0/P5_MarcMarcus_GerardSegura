62a0f0f0b5db4d34a00419844ac08731fca62d30
retr0 <gseguraa@edu.tecnocampus.cat>
2024-12-02 15:43:16 +0100

    me vuelvo loca, tornem a començar

--- src/greedy/SolucióVoraç.java
+++ src/greedy/SolucióVoraç.java
@@ -1,6 +1,7 @@
 package greedy;
 
 import estructura.Encreuades;
+import estructura.PosicioInicial;
 
 public class SolucióVoraç {
     /* TODO
@@ -20,7 +21,7 @@ public class SolucióVoraç {
         return null; // TODO
     }
 
-    private char[][] greedy() {
+    private char[][] greedy(){
         // mireu l'esquema utilitzat per la professora Lina
         // si voleu podeu modificar la capçalera d'aquest mètode
         // si voleu podeu utilitzar recursivitat
