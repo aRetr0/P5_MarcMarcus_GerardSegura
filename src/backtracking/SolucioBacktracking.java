a66f2310436a9dce69fa1a9743b3addcd344a501
retr0 <gseguraa@edu.tecnocampus.cat>
2024-12-02 16:52:19 +0100

    sigo enferma de la cabeza, nomes funciona 1solucionumeros

--- src/backtracking/SolucioBacktracking.java
+++ src/backtracking/SolucioBacktracking.java
@@ -1,100 +1,157 @@
 package backtracking;
+
 import estructura.Encreuades;
 import estructura.PosicioInicial;
 
 public class SolucioBacktracking {
 
-	/* TODO
-	 * cal definir els atributs necessaris
-	 */
-	private final Encreuades repte;
+    private final Encreuades repte;
+    private char[][] solucioActual;
+    private char[][] millorSolucio;
+    private int millorValor;
 
-	
-	public SolucioBacktracking(Encreuades repte) {
-		this.repte = repte;
-	}
+    public SolucioBacktracking(Encreuades repte) {
+        this.repte = repte;
+        this.solucioActual = repte.getPuzzle();
+        this.millorSolucio = null;
+        this.millorValor = 0;
+    }
 
-	public char[][] getMillorSolucio() {
-		return null; //TODO
-	}
+    public char[][] getMillorSolucio() {
+        return millorSolucio;
+    }
 
-	public Runnable start(boolean optim)
-	{
-		/* TODO
-		 * cal inicialitzar els atributs necessaris
-		 */
+    public Runnable start(boolean optim) {
+        if (!optim) {
+            if (!this.backUnaSolucio(0)) throw new RuntimeException("solució no trobada");
+            guardarMillorSolucio();
+        } else {
+            this.backMillorSolucio(0);
+        }
+        return null;
+    }
 
-		if(!optim) {
-			if (!this.backUnaSolucio(0))
-				throw new RuntimeException("solució no trobada");
-			guardarMillorSolucio();
-		}else
-			this.backMillorSolucio(0);
-		return null;
-	}
+    private boolean backUnaSolucio(int indexUbicacio) {
+        if (indexUbicacio >= repte.getEspaisDisponibles().size()) {
+            return true;
+        }
+        for (int indexItem = 0; indexItem < this.repte.getItemsSize(); indexItem++) {
+            if (acceptable(indexUbicacio, indexItem)) {
+                anotarASolucio(indexUbicacio, indexItem);
+                if (backUnaSolucio(indexUbicacio + 1)) {
+                    return true;
+                }
+                desanotarDeSolucio(indexUbicacio, indexItem);
+            }
+        }
+        return false;
+    }
 
-	/* esquema recursiu que troba una solució
-	 * utilitzem una variable booleana (que retornem)
-	 * per aturar el recorregut quan haguem trobat una solució
-	 */
-	private boolean backUnaSolucio(int indexUbicacio) {
-		boolean trobada = false;
-		// iterem sobre els possibles elements
-		for(int indexItem = 0; indexItem < this.repte.getItemsSize() && !trobada; indexItem++) {
-			//mirem si l'element es pot posar a la ubicació actual
-			if(acceptable( indexUbicacio, indexItem)) {
-				//posem l'element a la solució actual
-				anotarASolucio(indexUbicacio, indexItem);
+    private void backMillorSolucio(int indexUbicacio) {
+        if (indexUbicacio >= repte.getEspaisDisponibles().size()) {
+            int valorActual = calcularFuncioObjectiu(solucioActual);
+            if (valorActual > millorValor) {
+                millorValor = valorActual;
+                guardarMillorSolucio();
+            }
+            return;
+        }
+        for (int indexItem = 0; indexItem < this.repte.getItemsSize(); indexItem++) {
+            if (acceptable(indexUbicacio, indexItem)) {
+                anotarASolucio(indexUbicacio, indexItem);
+                backMillorSolucio(indexUbicacio + 1);
+                desanotarDeSolucio(indexUbicacio, indexItem);
+            }
+        }
+    }
 
-				if(esSolucio(indexUbicacio)) { // és solució si totes les ubicacions estan plenes
-					return true;
-				} else
-					trobada = this.backUnaSolucio(indexUbicacio+1); //inserim la següent paraula
-				if(!trobada)
-					// esborrem la paraula actual, per després posar-la a una altra ubicació
-					desanotarDeSolucio(indexUbicacio, indexItem);
-			}
-		}
-		return trobada;
-	}
-	/* TODO
-	 * Esquema recursiu que busca totes les solucions
-	 * no cal utilitzar una variable booleana per aturar perquè busquem totes les solucions
-	 * cal guardar una COPIA de la millor solució a una variable
-	 */
-	private void backMillorSolucio(int indexUbicacio) {
+    private boolean acceptable(int indexUbicacio, int indexItem) {
+        PosicioInicial pos = repte.getEspaisDisponibles().get(indexUbicacio);
+        char[] item = repte.getItem(indexItem);
+        int row = pos.getInitRow();
+        int col = pos.getInitCol();
+        int length = pos.getLength();
+        char direccio = pos.getDireccio();
 
-	}
-	
-	private boolean acceptable(int indexUbicacio, int indexItem) {
-		return false; //TODO
-	}
-	
-	private void anotarASolucio(int indexUbicacio, int indexItem) {
-		//TODO
-	}
-	
-	private void desanotarDeSolucio(int indexUbicacio, int indexItem) {
-		//TODO
-	}
+        if (item.length > length) return false;
 
-	
-	private boolean esSolucio(int index) {
-		return false; //TODO
-	}
-	
-	private int calcularFuncioObjectiu(char[][] matriu) {
-		return 0; //TODO
-	}
-	
-	private void guardarMillorSolucio() {
-		// TODO - cal guardar un clone
-	}
-	
-	public String toString() {
-		String resultat = "";
-		//TODO
-		return resultat;
-	}
+        for (int i = 0; i < item.length; i++) {
+            if (direccio == 'H') {
+                if (solucioActual[row][col + i] != ' ' && solucioActual[row][col + i] != item[i]) return false;
+            } else {
+                if (solucioActual[row + i][col] != ' ' && solucioActual[row + i][col] != item[i]) return false;
+            }
+        }
+        return true;
+    }
 
+    private void anotarASolucio(int indexUbicacio, int indexItem) {
+        PosicioInicial pos = repte.getEspaisDisponibles().get(indexUbicacio);
+        char[] item = repte.getItem(indexItem);
+        int row = pos.getInitRow();
+        int col = pos.getInitCol();
+        char direccio = pos.getDireccio();
+
+        for (int i = 0; i < item.length; i++) {
+            if (direccio == 'H') {
+                solucioActual[row][col + i] = item[i];
+            } else {
+                solucioActual[row + i][col] = item[i];
+            }
+        }
+    }
+
+    private void desanotarDeSolucio(int indexUbicacio, int indexItem) {
+        PosicioInicial pos = repte.getEspaisDisponibles().get(indexUbicacio);
+        char[] item = repte.getItem(indexItem);
+        int row = pos.getInitRow();
+        int col = pos.getInitCol();
+        char direccio = pos.getDireccio();
+
+        for (int i = 0; i < item.length; i++) {
+            if (direccio == 'H') {
+                if (esborrable(row, col + i, item[i])) solucioActual[row][col + i] = ' ';
+            } else {
+                if (esborrable(row + i, col, item[i])) solucioActual[row + i][col] = ' ';
+            }
+        }
+    }
+
+    private boolean esborrable(int row, int col, char c) {
+        return solucioActual[row][col] == c;
+    }
+
+    private boolean esSolucio(int index) {
+        return index == repte.getEspaisDisponibles().size();
+    }
+
+    private int calcularFuncioObjectiu(char[][] matriu) {
+        int valor = 0;
+        for (char[] fila : matriu) {
+            for (char c : fila) {
+                if (c != ' ' && c != '▪') {
+                    valor += c;
+                }
+            }
+        }
+        return valor;
+    }
+
+    private void guardarMillorSolucio() {
+        millorSolucio = new char[solucioActual.length][];
+        for (int i = 0; i < solucioActual.length; i++) {
+            millorSolucio[i] = solucioActual[i].clone();
+        }
+    }
+
+    public String toString() {
+        StringBuilder resultat = new StringBuilder();
+        for (char[] fila : solucioActual) {
+            for (char c : fila) {
+                resultat.append(c).append(' ');
+            }
+            resultat.append('\n');
+        }
+        return resultat.toString();
+    }
 }
